9a442ac664f3b2c3a16e04da5ea0fe8c9a99c78d
BRMO <brandommoore@gmail.com>
2023-08-03 12:10:54 +0200

    HomeView updated and added topBar

---
+++ Project/Idearium/Idearium/Presentation/DetailView/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  Idearium
+//
+//  Created by Córdoba Velicias Manuel on 3/8/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("DetailView")
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
--- Project/Idearium/Idearium/Presentation/Home/HomeView.swift
+++ Project/Idearium/Idearium/Presentation/Home/HomeView.swift
@@ -13,23 +13,34 @@ struct HomeView: View {
 		// MARK: - Vars
 		let bgcol = Color("AppBackgroundColor")
 		
-		NavigationStack {
-			List {
-				ArtworkCard(image: Image("astronaut_horse"), prompt: "An astronaut riding a horse on Mars")
-				ArtworkCard(image: Image("astronaut_horse"), prompt: "An astronaut riding a horse on Mars")
-				ArtworkCard(image: Image("astronaut_horse"), prompt: "An astronaut riding a horse on Mars")
+		VStack(alignment: .leading, spacing: 0) {
+			Image("Idearium_text")
+				.resizable()
+				.frame(width: 163 * 0.75, height: 30 * 0.75)
+				.padding([.leading, .trailing], 35)
+				.padding(.top, 10)
+				.padding(.bottom, 20)
+				.background(bgcol)
+			NavigationStack {
+				List {
+					ArtworkCard(image: Image("astronaut_horse"), prompt: "An astronaut riding a horse on Mars")
+					ArtworkCard(image: Image("astronaut_horse"), prompt: "An astronaut riding a horse on Mars")
+					ArtworkCard(image: Image("astronaut_horse"), prompt: "An astronaut riding a horse on Mars")
+				}
+				.scrollIndicators(.hidden)
+				.padding([.leading, .trailing], 10)
+				.padding(.top, -30)
+				.ignoresSafeArea(.all)
+				.listRowSeparatorTint(bgcol)
+				.scrollContentBackground(.hidden)
+				.background(bgcol)
 			}
-			.scrollIndicators(.hidden)
-			.padding(10)
 			.ignoresSafeArea(.all)
-			.listRowSeparatorTint(bgcol)
 			.scrollContentBackground(.hidden)
+			.preferredColorScheme(.dark)
 			.background(bgcol)
-			
 		}
-		.ignoresSafeArea(.all)
-		.scrollContentBackground(.hidden)
-		.preferredColorScheme(.dark)
+		.background(bgcol)
 	}
 }
 
